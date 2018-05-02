7bfe96e66de061d849ff19197d0a946301263df5
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-02 03:13:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ const fareDoubler = createFareMultiplier(2)
 
 const fareTripler = createFareMultiplier(3)
 
-const selectDifferentDrivers = function (drivers, ) {
-  
+const selectDifferentDrivers = function (drivers, fn) {
+  return fn
 }
