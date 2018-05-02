770ef5a9e4f55cce4d3978f5b7c6929148b1991e
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-02 02:54:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const returnFirstTwoDrivers = function (driver) {
 const returnLastTwoDrivers = function (driver) {
   return driver.slice(-2)
 }
+
+const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()]
